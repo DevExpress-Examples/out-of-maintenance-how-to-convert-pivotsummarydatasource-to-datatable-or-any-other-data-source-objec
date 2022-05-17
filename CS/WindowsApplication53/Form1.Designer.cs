ef2161c2fe5faab25d929a87531ffaa8982c0778
ivan.kozhenov <ivan.kozhenov@devexpress.com>
2022-05-17 13:33:36 +0400

    Migration to Optimized mode
    Migration to Optimized mode and new Data Bindings

--- CS/WindowsApplication53/Form1.Designer.cs
+++ CS/WindowsApplication53/Form1.Designer.cs
@@ -28,24 +28,31 @@ namespace WindowsApplication53
         /// </summary>
         private void InitializeComponent()
         {
+            DevExpress.XtraPivotGrid.DataSourceColumnBinding dataSourceColumnBinding1 = new DevExpress.XtraPivotGrid.DataSourceColumnBinding();
+            DevExpress.XtraPivotGrid.DataSourceColumnBinding dataSourceColumnBinding2 = new DevExpress.XtraPivotGrid.DataSourceColumnBinding();
+            DevExpress.XtraPivotGrid.DataSourceColumnBinding dataSourceColumnBinding3 = new DevExpress.XtraPivotGrid.DataSourceColumnBinding();
+            DevExpress.XtraPivotGrid.DataSourceColumnBinding dataSourceColumnBinding4 = new DevExpress.XtraPivotGrid.DataSourceColumnBinding();
+            DevExpress.XtraPivotGrid.DataSourceColumnBinding dataSourceColumnBinding5 = new DevExpress.XtraPivotGrid.DataSourceColumnBinding();
+            DevExpress.XtraPivotGrid.DataSourceColumnBinding dataSourceColumnBinding6 = new DevExpress.XtraPivotGrid.DataSourceColumnBinding();
             this.pivotGridControl1 = new DevExpress.XtraPivotGrid.PivotGridControl();
             this.dataSet1 = new System.Data.DataSet();
             this.dataTable1 = new System.Data.DataTable();
             this.dataColumn1 = new System.Data.DataColumn();
             this.dataColumn2 = new System.Data.DataColumn();
             this.dataColumn3 = new System.Data.DataColumn();
-            this.fieldName = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.fieldName1 = new DevExpress.XtraPivotGrid.PivotGridField();
             this.fieldYear = new DevExpress.XtraPivotGrid.PivotGridField();
             this.fieldValueSum = new DevExpress.XtraPivotGrid.PivotGridField();
             this.fieldValueCount = new DevExpress.XtraPivotGrid.PivotGridField();
             this.fieldValueAverage = new DevExpress.XtraPivotGrid.PivotGridField();
-            this.fieldDate = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.fieldDate1 = new DevExpress.XtraPivotGrid.PivotGridField();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             ((System.ComponentModel.ISupportInitialize)(this.pivotGridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
@@ -58,14 +65,15 @@ namespace WindowsApplication53
             this.pivotGridControl1.DataSource = this.dataSet1;
             this.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pivotGridControl1.Fields.AddRange(new DevExpress.XtraPivotGrid.PivotGridField[] {
-            this.fieldName,
+            this.fieldName1,
             this.fieldYear,
             this.fieldValueSum,
             this.fieldValueCount,
             this.fieldValueAverage,
-            this.fieldDate});
+            this.fieldDate1});
             this.pivotGridControl1.Location = new System.Drawing.Point(0, 0);
             this.pivotGridControl1.Name = "pivotGridControl1";
+            this.pivotGridControl1.OptionsData.DataProcessingEngine = DevExpress.XtraPivotGrid.PivotDataProcessingEngine.Optimized;
             this.pivotGridControl1.OptionsDataField.FieldNaming = DevExpress.XtraPivotGrid.DataFieldNaming.Name;
             this.pivotGridControl1.Size = new System.Drawing.Size(412, 403);
             this.pivotGridControl1.TabIndex = 0;
@@ -98,29 +106,31 @@ namespace WindowsApplication53
             this.dataColumn3.ColumnName = "Value";
             this.dataColumn3.DataType = typeof(decimal);
             // 
-            // fieldName
+            // fieldName1
             // 
-            this.fieldName.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea;
-            this.fieldName.AreaIndex = 0;
-            this.fieldName.FieldName = "Name";
-            this.fieldName.Name = "fieldName";
+            this.fieldName1.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea;
+            this.fieldName1.AreaIndex = 0;
+            dataSourceColumnBinding1.ColumnName = "Name";
+            this.fieldName1.DataBinding = dataSourceColumnBinding1;
+            this.fieldName1.Name = "fieldName1";
             // 
             // fieldYear
             // 
             this.fieldYear.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea;
             this.fieldYear.AreaIndex = 0;
             this.fieldYear.Caption = "Year";
-            this.fieldYear.FieldName = "Date";
-            this.fieldYear.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateYear;
+            dataSourceColumnBinding2.ColumnName = "Date";
+            dataSourceColumnBinding2.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateYear;
+            this.fieldYear.DataBinding = dataSourceColumnBinding2;
             this.fieldYear.Name = "fieldYear";
-            this.fieldYear.UnboundFieldName = "fieldDate";
             // 
             // fieldValueSum
             // 
             this.fieldValueSum.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea;
             this.fieldValueSum.AreaIndex = 0;
             this.fieldValueSum.Caption = "Sum";
-            this.fieldValueSum.FieldName = "Value";
+            dataSourceColumnBinding3.ColumnName = "Value";
+            this.fieldValueSum.DataBinding = dataSourceColumnBinding3;
             this.fieldValueSum.Name = "fieldValueSum";
             // 
             // fieldValueCount
@@ -128,28 +138,28 @@ namespace WindowsApplication53
             this.fieldValueCount.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea;
             this.fieldValueCount.AreaIndex = 1;
             this.fieldValueCount.Caption = "Count";
-            this.fieldValueCount.FieldName = "Value";
+            dataSourceColumnBinding4.ColumnName = "Value";
+            this.fieldValueCount.DataBinding = dataSourceColumnBinding4;
             this.fieldValueCount.Name = "fieldValueCount";
-            this.fieldValueCount.SummaryType = DevExpress.Data.PivotGrid.PivotSummaryType.Count;
             // 
             // fieldValueAverage
             // 
             this.fieldValueAverage.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea;
             this.fieldValueAverage.AreaIndex = 2;
             this.fieldValueAverage.Caption = "Average";
-            this.fieldValueAverage.FieldName = "Value";
+            dataSourceColumnBinding5.ColumnName = "Value";
+            this.fieldValueAverage.DataBinding = dataSourceColumnBinding5;
             this.fieldValueAverage.Name = "fieldValueAverage";
-            this.fieldValueAverage.SummaryType = DevExpress.Data.PivotGrid.PivotSummaryType.Average;
             // 
-            // fieldDate
+            // fieldDate1
             // 
-            this.fieldDate.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea;
-            this.fieldDate.AreaIndex = 1;
-            this.fieldDate.Caption = "Date";
-            this.fieldDate.FieldName = "Date";
-            this.fieldDate.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.Date;
-            this.fieldDate.Name = "fieldDate";
-            this.fieldDate.UnboundFieldName = "fieldDate1";
+            this.fieldDate1.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea;
+            this.fieldDate1.AreaIndex = 1;
+            this.fieldDate1.Caption = "Date";
+            dataSourceColumnBinding6.ColumnName = "Date";
+            dataSourceColumnBinding6.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.Date;
+            this.fieldDate1.DataBinding = dataSourceColumnBinding6;
+            this.fieldDate1.Name = "fieldDate1";
             // 
             // dataGridView1
             // 
@@ -192,6 +202,7 @@ namespace WindowsApplication53
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -205,12 +216,12 @@ namespace WindowsApplication53
         private System.Data.DataColumn dataColumn1;
         private System.Data.DataColumn dataColumn2;
         private System.Data.DataColumn dataColumn3;
-        private DevExpress.XtraPivotGrid.PivotGridField fieldName;
+        private DevExpress.XtraPivotGrid.PivotGridField fieldName1;
         private DevExpress.XtraPivotGrid.PivotGridField fieldYear;
         private DevExpress.XtraPivotGrid.PivotGridField fieldValueSum;
         private DevExpress.XtraPivotGrid.PivotGridField fieldValueCount;
         private DevExpress.XtraPivotGrid.PivotGridField fieldValueAverage;
-        private DevExpress.XtraPivotGrid.PivotGridField fieldDate;
+        private DevExpress.XtraPivotGrid.PivotGridField fieldDate1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.SplitContainer splitContainer1;
     }
--- VB/WindowsApplication53/Form1.Designer.vb
+++ VB/WindowsApplication53/Form1.Designer.vb
@@ -25,179 +25,189 @@ Namespace WindowsApplication53
 		''' the contents of this method with the code editor.
 		''' </summary>
 		Private Sub InitializeComponent()
-			Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
-			Me.dataSet1 = New System.Data.DataSet()
-			Me.dataTable1 = New System.Data.DataTable()
-			Me.dataColumn1 = New System.Data.DataColumn()
-			Me.dataColumn2 = New System.Data.DataColumn()
-			Me.dataColumn3 = New System.Data.DataColumn()
-			Me.fieldName = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldYear = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldValueSum = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldValueCount = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldValueAverage = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldDate = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.dataGridView1 = New System.Windows.Forms.DataGridView()
-			Me.splitContainer1 = New System.Windows.Forms.SplitContainer()
-			CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.dataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.dataTable1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.dataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.splitContainer1.Panel1.SuspendLayout()
-			Me.splitContainer1.Panel2.SuspendLayout()
-			Me.splitContainer1.SuspendLayout()
-			Me.SuspendLayout()
-			' 
-			' pivotGridControl1
-			' 
-			Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.Default
-			Me.pivotGridControl1.DataMember = "Data"
-			Me.pivotGridControl1.DataSource = Me.dataSet1
-			Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() { Me.fieldName, Me.fieldYear, Me.fieldValueSum, Me.fieldValueCount, Me.fieldValueAverage, Me.fieldDate})
-			Me.pivotGridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.pivotGridControl1.Name = "pivotGridControl1"
-			Me.pivotGridControl1.OptionsDataField.FieldNaming = DevExpress.XtraPivotGrid.DataFieldNaming.Name
-			Me.pivotGridControl1.Size = New System.Drawing.Size(412, 403)
-			Me.pivotGridControl1.TabIndex = 0
-			' 
-			' dataSet1
-			' 
-			Me.dataSet1.DataSetName = "NewDataSet"
-			Me.dataSet1.Tables.AddRange(New System.Data.DataTable() { Me.dataTable1})
-			' 
-			' dataTable1
-			' 
-			Me.dataTable1.Columns.AddRange(New System.Data.DataColumn() { Me.dataColumn1, Me.dataColumn2, Me.dataColumn3})
-			Me.dataTable1.TableName = "Data"
-			' 
-			' dataColumn1
-			' 
-			Me.dataColumn1.ColumnName = "Name"
-			' 
-			' dataColumn2
-			' 
-			Me.dataColumn2.ColumnName = "Date"
-			Me.dataColumn2.DataType = GetType(System.DateTime)
-			' 
-			' dataColumn3
-			' 
-			Me.dataColumn3.ColumnName = "Value"
-			Me.dataColumn3.DataType = GetType(Decimal)
-			' 
-			' fieldName
-			' 
-			Me.fieldName.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
-			Me.fieldName.AreaIndex = 0
-			Me.fieldName.FieldName = "Name"
-			Me.fieldName.Name = "fieldName"
-			' 
-			' fieldYear
-			' 
-			Me.fieldYear.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
-			Me.fieldYear.AreaIndex = 0
-			Me.fieldYear.Caption = "Year"
-			Me.fieldYear.FieldName = "Date"
-			Me.fieldYear.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateYear
-			Me.fieldYear.Name = "fieldYear"
-			Me.fieldYear.UnboundFieldName = "fieldDate"
-			' 
-			' fieldValueSum
-			' 
-			Me.fieldValueSum.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
-			Me.fieldValueSum.AreaIndex = 0
-			Me.fieldValueSum.Caption = "Sum"
-			Me.fieldValueSum.FieldName = "Value"
-			Me.fieldValueSum.Name = "fieldValueSum"
-			' 
-			' fieldValueCount
-			' 
-			Me.fieldValueCount.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
-			Me.fieldValueCount.AreaIndex = 1
-			Me.fieldValueCount.Caption = "Count"
-			Me.fieldValueCount.FieldName = "Value"
-			Me.fieldValueCount.Name = "fieldValueCount"
-			Me.fieldValueCount.SummaryType = DevExpress.Data.PivotGrid.PivotSummaryType.Count
-			' 
-			' fieldValueAverage
-			' 
-			Me.fieldValueAverage.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
-			Me.fieldValueAverage.AreaIndex = 2
-			Me.fieldValueAverage.Caption = "Average"
-			Me.fieldValueAverage.FieldName = "Value"
-			Me.fieldValueAverage.Name = "fieldValueAverage"
-			Me.fieldValueAverage.SummaryType = DevExpress.Data.PivotGrid.PivotSummaryType.Average
-			' 
-			' fieldDate
-			' 
-			Me.fieldDate.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
-			Me.fieldDate.AreaIndex = 1
-			Me.fieldDate.Caption = "Date"
-			Me.fieldDate.FieldName = "Date"
-			Me.fieldDate.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.Date
-			Me.fieldDate.Name = "fieldDate"
-			Me.fieldDate.UnboundFieldName = "fieldDate1"
-			' 
-			' dataGridView1
-			' 
-			Me.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-			Me.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.dataGridView1.Location = New System.Drawing.Point(0, 0)
-			Me.dataGridView1.Name = "dataGridView1"
-			Me.dataGridView1.Size = New System.Drawing.Size(543, 403)
-			Me.dataGridView1.TabIndex = 1
-			' 
-			' splitContainer1
-			' 
-			Me.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.splitContainer1.Location = New System.Drawing.Point(0, 0)
-			Me.splitContainer1.Name = "splitContainer1"
-			' 
-			' splitContainer1.Panel1
-			' 
-			Me.splitContainer1.Panel1.Controls.Add(Me.pivotGridControl1)
-			' 
-			' splitContainer1.Panel2
-			' 
-			Me.splitContainer1.Panel2.Controls.Add(Me.dataGridView1)
-			Me.splitContainer1.Size = New System.Drawing.Size(959, 403)
-			Me.splitContainer1.SplitterDistance = 412
-			Me.splitContainer1.TabIndex = 2
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(959, 403)
-			Me.Controls.Add(Me.splitContainer1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.dataSet1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.dataTable1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.dataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.splitContainer1.Panel1.ResumeLayout(False)
-			Me.splitContainer1.Panel2.ResumeLayout(False)
-			Me.splitContainer1.ResumeLayout(False)
-			Me.ResumeLayout(False)
+            Dim DataSourceColumnBinding1 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim DataSourceColumnBinding2 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim DataSourceColumnBinding3 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim DataSourceColumnBinding4 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim DataSourceColumnBinding5 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim DataSourceColumnBinding6 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
+            Me.dataSet1 = New System.Data.DataSet()
+            Me.dataTable1 = New System.Data.DataTable()
+            Me.dataColumn1 = New System.Data.DataColumn()
+            Me.dataColumn2 = New System.Data.DataColumn()
+            Me.dataColumn3 = New System.Data.DataColumn()
+            Me.fieldName1 = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldYear = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldValueSum = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldValueCount = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldValueAverage = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldDate1 = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.dataGridView1 = New System.Windows.Forms.DataGridView()
+            Me.splitContainer1 = New System.Windows.Forms.SplitContainer()
+            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.dataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.dataTable1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.dataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.splitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.splitContainer1.Panel1.SuspendLayout()
+            Me.splitContainer1.Panel2.SuspendLayout()
+            Me.splitContainer1.SuspendLayout()
+            Me.SuspendLayout()
+            '
+            'pivotGridControl1
+            '
+            Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.Default
+            Me.pivotGridControl1.DataMember = "Data"
+            Me.pivotGridControl1.DataSource = Me.dataSet1
+            Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() {Me.fieldName1, Me.fieldYear, Me.fieldValueSum, Me.fieldValueCount, Me.fieldValueAverage, Me.fieldDate1})
+            Me.pivotGridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.pivotGridControl1.Name = "pivotGridControl1"
+            Me.pivotGridControl1.OptionsData.DataProcessingEngine = DevExpress.XtraPivotGrid.PivotDataProcessingEngine.Optimized
+            Me.pivotGridControl1.OptionsDataField.FieldNaming = DevExpress.XtraPivotGrid.DataFieldNaming.Name
+            Me.pivotGridControl1.Size = New System.Drawing.Size(412, 403)
+            Me.pivotGridControl1.TabIndex = 0
+            '
+            'dataSet1
+            '
+            Me.dataSet1.DataSetName = "NewDataSet"
+            Me.dataSet1.Tables.AddRange(New System.Data.DataTable() {Me.dataTable1})
+            '
+            'dataTable1
+            '
+            Me.dataTable1.Columns.AddRange(New System.Data.DataColumn() {Me.dataColumn1, Me.dataColumn2, Me.dataColumn3})
+            Me.dataTable1.TableName = "Data"
+            '
+            'dataColumn1
+            '
+            Me.dataColumn1.ColumnName = "Name"
+            '
+            'dataColumn2
+            '
+            Me.dataColumn2.ColumnName = "Date"
+            Me.dataColumn2.DataType = GetType(Date)
+            '
+            'dataColumn3
+            '
+            Me.dataColumn3.ColumnName = "Value"
+            Me.dataColumn3.DataType = GetType(Decimal)
+            '
+            'fieldName1
+            '
+            Me.fieldName1.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
+            Me.fieldName1.AreaIndex = 0
+            DataSourceColumnBinding1.ColumnName = "Name"
+            Me.fieldName1.DataBinding = DataSourceColumnBinding1
+            Me.fieldName1.Name = "fieldName1"
+            '
+            'fieldYear
+            '
+            Me.fieldYear.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
+            Me.fieldYear.AreaIndex = 0
+            Me.fieldYear.Caption = "Year"
+            DataSourceColumnBinding2.ColumnName = "Date"
+            DataSourceColumnBinding2.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateYear
+            Me.fieldYear.DataBinding = DataSourceColumnBinding2
+            Me.fieldYear.Name = "fieldYear"
+            '
+            'fieldValueSum
+            '
+            Me.fieldValueSum.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
+            Me.fieldValueSum.AreaIndex = 0
+            Me.fieldValueSum.Caption = "Sum"
+            DataSourceColumnBinding3.ColumnName = "Value"
+            Me.fieldValueSum.DataBinding = DataSourceColumnBinding3
+            Me.fieldValueSum.Name = "fieldValueSum"
+            '
+            'fieldValueCount
+            '
+            Me.fieldValueCount.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
+            Me.fieldValueCount.AreaIndex = 1
+            Me.fieldValueCount.Caption = "Count"
+            DataSourceColumnBinding4.ColumnName = "Value"
+            Me.fieldValueCount.DataBinding = DataSourceColumnBinding4
+            Me.fieldValueCount.Name = "fieldValueCount"
+            '
+            'fieldValueAverage
+            '
+            Me.fieldValueAverage.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
+            Me.fieldValueAverage.AreaIndex = 2
+            Me.fieldValueAverage.Caption = "Average"
+            DataSourceColumnBinding5.ColumnName = "Value"
+            Me.fieldValueAverage.DataBinding = DataSourceColumnBinding5
+            Me.fieldValueAverage.Name = "fieldValueAverage"
+            '
+            'fieldDate1
+            '
+            Me.fieldDate1.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
+            Me.fieldDate1.AreaIndex = 1
+            Me.fieldDate1.Caption = "Date"
+            DataSourceColumnBinding6.ColumnName = "Date"
+            DataSourceColumnBinding6.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.[Date]
+            Me.fieldDate1.DataBinding = DataSourceColumnBinding6
+            Me.fieldDate1.Name = "fieldDate1"
+            '
+            'dataGridView1
+            '
+            Me.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+            Me.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.dataGridView1.Location = New System.Drawing.Point(0, 0)
+            Me.dataGridView1.Name = "dataGridView1"
+            Me.dataGridView1.Size = New System.Drawing.Size(543, 403)
+            Me.dataGridView1.TabIndex = 1
+            '
+            'splitContainer1
+            '
+            Me.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.splitContainer1.Location = New System.Drawing.Point(0, 0)
+            Me.splitContainer1.Name = "splitContainer1"
+            '
+            'splitContainer1.Panel1
+            '
+            Me.splitContainer1.Panel1.Controls.Add(Me.pivotGridControl1)
+            '
+            'splitContainer1.Panel2
+            '
+            Me.splitContainer1.Panel2.Controls.Add(Me.dataGridView1)
+            Me.splitContainer1.Size = New System.Drawing.Size(959, 403)
+            Me.splitContainer1.SplitterDistance = 412
+            Me.splitContainer1.TabIndex = 2
+            '
+            'Form1
+            '
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(959, 403)
+            Me.Controls.Add(Me.splitContainer1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.dataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.dataTable1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.dataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.splitContainer1.Panel1.ResumeLayout(False)
+            Me.splitContainer1.Panel2.ResumeLayout(False)
+            CType(Me.splitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.splitContainer1.ResumeLayout(False)
+            Me.ResumeLayout(False)
 
-		End Sub
+        End Sub
 
-		#End Region
+#End Region
 
-		Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
+        Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
 		Private dataSet1 As System.Data.DataSet
 		Private dataTable1 As System.Data.DataTable
 		Private dataColumn1 As System.Data.DataColumn
 		Private dataColumn2 As System.Data.DataColumn
 		Private dataColumn3 As System.Data.DataColumn
-		Private fieldName As DevExpress.XtraPivotGrid.PivotGridField
+		Private fieldName1 As DevExpress.XtraPivotGrid.PivotGridField
 		Private fieldYear As DevExpress.XtraPivotGrid.PivotGridField
 		Private fieldValueSum As DevExpress.XtraPivotGrid.PivotGridField
 		Private fieldValueCount As DevExpress.XtraPivotGrid.PivotGridField
 		Private fieldValueAverage As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldDate As DevExpress.XtraPivotGrid.PivotGridField
+		Private fieldDate1 As DevExpress.XtraPivotGrid.PivotGridField
 		Private dataGridView1 As System.Windows.Forms.DataGridView
 		Private splitContainer1 As System.Windows.Forms.SplitContainer
 	End Class
